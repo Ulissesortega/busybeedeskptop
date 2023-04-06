4986b80fc83ee797a7b44acdb71e788f3f0ddf2d
Ulises <ulisses.ortega@hotmail.com>
2023-04-06 13:35:48 -0700

    update

--- src/Components/MainLogin.tsx
+++ src/Components/MainLogin.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import '../App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function MainLogin() {
   return (
