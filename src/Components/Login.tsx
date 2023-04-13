40d598d0b15cc3f710b3f0d455e112665a06a6fa
Ulises <ulisses.ortega@hotmail.com>
2023-04-13 14:35:00 -0700

    update

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -6,9 +6,16 @@ export default function Login() {
   return (
     <Container className='bgColor test'>
       <Row className='mt-5'>
+        
         <Col>Welcome to Busy Bee</Col>
+
+
+        <Col> <Col xl={1}> <div className="divider d-none d-sm-block d-md-none d-lg-block d-xl-block"></div> </Col></Col>
+        
+        
+        
         <Col>Welcome to Busy Bee</Col>
-        <Col>Welcome to Busy Bee</Col>
+      
       </Row>
     </Container>
   );
