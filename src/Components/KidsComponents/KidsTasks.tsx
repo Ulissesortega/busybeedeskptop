c91b083aabc5196627f68b9528f99d08ec5ef6b1
UlissesOrtega <51983586+Ulissesortega@users.noreply.github.com>
2023-05-17 14:58:31 -0700

    update

--- src/Components/KidsComponents/KidsTasks.tsx
+++ src/Components/KidsComponents/KidsTasks.tsx
@@ -104,13 +104,13 @@ export default function KidsTasks() {
                 }
               })
             }
-          </Col>
-        </Row>
         <Row>
           <Col className='right-title mt-2'>
             <Link to="/KidsRewards">
               <button className='btn-format rounded-pill mt-3'>Check Rewards</button>
             </Link>
+          </Col>
+        </Row>
           </Col>
         </Row>
       </Container>
